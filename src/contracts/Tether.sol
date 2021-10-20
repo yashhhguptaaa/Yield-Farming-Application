ce4036ed7da87da2bc23c497e909e9e88025e45d
yash gupta <yash3144@gmail.com>
2021-10-20 15:02:22 +0530

    Create two events for Transfer and Approve

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -6,4 +6,16 @@ contract Tether {
     string public symbol = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000;  //1 million tokens 
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
 }
