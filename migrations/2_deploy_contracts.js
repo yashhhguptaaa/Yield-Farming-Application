d8399d58ef2b6d9df6f6d103a42675a80fd14a2d
yash gupta <yash3144@gmail.com>
2021-10-28 15:35:20 +0530

    Completed our Smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,20 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
-    await deployer.deploy(Tether)
+module.exports = async function(deployer, network, accounts) {
+    await deployer.deploy(Tether);
+    const tether = await Tether.deployed();
+
+    await deployer.deploy(RWD);
+    const rwd = await RWD.deployed(); 
+
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+    //Tranfer all RWD tokens to Decentral Bank
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000');
+
+    //Distribute 100 Tether tokens to investor
+    await tether.transfer(accounts[1], '100000000000000000000');
 };
