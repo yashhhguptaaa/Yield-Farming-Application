1ceb823454994e2f56b3208fd35c0ac2d21a8a08
yash gupta <yash3144@gmail.com>
2021-10-30 16:38:22 +0530

    Load bank png and account number

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Navbar from './Navbar';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account:'0x0'
+        }
+    }
+
     render() {
         return (
             <div>
-                <Navbar />
+                <Navbar account={this.state.account}/>
                 <div className="text-center">
                     <h>Hello, world!</h>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React, {Component} from "react";
+import bank from '../bank.png';
 
 class Navbar extends Component {
     render() {
         return (
             <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: 'black',height:'50px'}}>
-                <a style={{color:'white'}}>DAPP Yield Farming (Decentralized Banking)</a>
-                <ul>
-                    <li>
-                        <small style={{color: "white"}}>Account Number
+                <a className='navbar-brand col-sm-3 col-md-2 mr-0' 
+                style={{color:'white'}}> 
+                <img src={bank} style={{width:'50px', height:'30px'}} className='d-inline-block align-top' alt='bank image'/>
+                &nbsp; DAPP Yield Farming (Decentralized Banking)
+                </a>
+                <ul className='navbar-nav px-3'>
+                    <li className='text-nowrap d-none nav-item d-sm-none d-sm-block'>
+                        <small style={{color: "white"}}>ACCOUNT NUMBER: {this.props.account}
                         </small>
                     </li>
                 </ul>
