3e3a568079f8cc3bb423fb3d9908c118e05bf11f
yash gupta <yash3144@gmail.com>
2021-10-30 18:30:27 +0530

    Set the state of account

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
 import React, {Component} from "react";
 import './App.css';
 import Navbar from './Navbar';
+import Web3 from "web3";
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount(){
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        if(window.ethereum){
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('No ethereum browser detected! You can checkout MetaMask!')
+        }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts();
+        this.setState({account : account[0]})
+    }
 
     constructor(props) {
         super(props)
         this.state = {
-            account:'0x0'
+            account:'0x0',
+            tether:{},
+            rwd : {},
+            decentralBank: {},
+            tetherBalance: '0',
+            rwdBalance: '0',
+            stakingBalance: '0',
+            loading: true
         }
     }
+
+   
 
     render() {
         return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
             <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: 'black',height:'50px'}}>
                 <a className='navbar-brand col-sm-3 col-md-2 mr-0' 
                 style={{color:'white'}}> 
-                <img src={bank} style={{width:'50px', height:'30px'}} className='d-inline-block align-top' alt='bank image'/>
+                <img src={bank} style={{width:'50px', height:'30px'}} className='d-inline-block align-top' alt='bank'/>
                 &nbsp; DAPP Yield Farming (Decentralized Banking)
                 </a>
                 <ul className='navbar-nav px-3'>
